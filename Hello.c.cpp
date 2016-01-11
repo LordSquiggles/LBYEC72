ae5dce7843f060513477b5716a8e6a8ea38c9cd4
LordSquiggles <infernaldestruction@yahoo.com>
2016-01-11 13:31:05 +0800

    first commit

---
+++ Hello.c.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define PI 3.14159
+
+void printNameCourse();
+float computeCircleArea(float functionRadius);
+
+int main()
+{
+	printNameCourse();
+	
+	float area;
+	float radius;
+	
+	radius=10;
+	
+	area = computeCircleArea(radius);
+	printf("The area is in %f\n", area);
+	return EXIT_SUCCESS;
+}
+
+void printNameCourse(){
+
+
+puts("Choco Jose Antonio G.");
+puts("Hello LBYEC 72!");
+
+
+}
+
+float computeCircleArea(float functionRadius){
+	return PI * functionRadius * functionRadius;
+}
